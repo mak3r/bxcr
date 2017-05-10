3872f8991c4c9f75ab069487ff728c20475ffb09
Austin Parker <aparker@apprenda.com>
2017-05-10 14:00:14 -0400

    fixing typo in configuresql

--- ps/Configure-SQL.ps1
+++ ps/Configure-SQL.ps1
@@ -1,2 +1,2 @@
-sqlcmd -U sa -P "@bxcr@dm!Npaas" -S localhost -Q "create login bxcr with password=N'password', default_database=[master], check_expiration=off, check_policy=off"
-sqlcmd -U sa -P "@bxcr@dm!Npaas" -S localhost -Q "exec master..sp_addsrvrolemember @loginame = N'bxcr', @rolename = N'sysadmin'"
+sqlcmd -U sa -P "bxcr@dm!Npaas" -S localhost -Q "create login bxcr with password=N'password', default_database=[master], check_expiration=off, check_policy=off"
+sqlcmd -U sa -P "bxcr@dm!Npaas" -S localhost -Q "exec master..sp_addsrvrolemember @loginame = N'bxcr', @rolename = N'sysadmin'"
